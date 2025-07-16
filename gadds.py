748c5116ccaf18941b32be57f7d03a8bf3f9976f
yyrliu <70476688+yyrliu@users.noreply.github.com>
2025-07-16 09:10:34 -0700

    Translate comments to english

--- gadds.py
+++ gadds.py
@@ -44,10 +44,10 @@ class AreaDetectorImage(object):
             if np.issubdtype(image.data.dtype, np.integer) and np.min(image.data) >= 0:
                 image.data = image.data.astype(np.min_scalar_type(np.max(image.data)))
         self.image = image
-        self.alpha = np.nan  # 検出器の中心の2θ値 (単位：radian)
-        self.distance = np.nan  # 試料から検出面までの距離（単位：cm）
-        self.densityXY = (np.nan, np.nan)  # 単位長さ (cm) 辺りのピクセル数
-        self.centerXY = (np.nan, np.nan)  # 検出器中心のx, y座標（単位：ピクセル）。
+        self.alpha = np.nan  # 2-theta value at the center of the detector (unit: radian)
+        self.distance = np.nan  # Distance from sample to detector plane (unit: cm)
+        self.densityXY = (np.nan, np.nan)  # Number of pixels per unit length (cm)
+        self.centerXY = (np.nan, np.nan)  # x, y coordinates of the detector center (unit: pixels).
         self.scale = 1  # linear scale factor
         self.offset = 0  # linear offset
         self.limits = (np.nan, np.nan, np.nan, np.nan)  # min2θ, max2θ, minγ, maxγ
@@ -96,7 +96,7 @@ class AreaDetectorImage(object):
         2.3.4 Pixel Position in Diffraction Space -- Flat Detector
 
         """
-        # 2θ=90°でのtan(2θ)の発散対策
+        # Countermeasure for divergence of tan(2θ) at 2θ=90°
         if np.isscalar(twoth):
             if np.isclose(twoth, np.pi/2, atol=1e-6, rtol=0):
                 twoth += 1e-6
@@ -111,13 +111,13 @@ class AreaDetectorImage(object):
             (cos(alpha) - sin(alpha)*tan(twoth)*sin(gamma))
         y = -(x*sin(alpha) + D*cos(alpha)) * tan(twoth) * cos(gamma)
 
-        # (x, y) = (0, 0) は図の左下に、(row, col) = (0, 0) は左上に対応する。
-        # x と col、yとrowがそれぞれ対応する（とてもややこしい）。
-        return (self.image.dim2 - y * self.densityXY[1] - self.centerXY[1],
+        # (x, y) corresponds to the lower left of the diagram, and (row, col) = (0, 0) corresponds to the upper left.
+        # x corresponds to col, and y corresponds to row (very confusing).
+        return (self.image.shape[-2] - y * self.densityXY[1] - self.centerXY[1],
                 x * self.densityXY[0] + self.centerXY[0])
 
     def relim(self):
-        rr, cc = np.indices((self.image.dim2, self.image.dim1))
+        rr, cc = np.indices((self.image.shape[-2], self.image.shape[-1]))
         twoth, gamma = self.rowcol_to_angles(rr, cc)
         if twoth.size > 0 and gamma.size > 0:
             self.limits = (np.min(twoth), np.max(twoth), np.min(gamma), np.max(gamma))
@@ -127,9 +127,9 @@ class AreaDetectorImage(object):
         if self.image.data is None:
             raise ValueError('Cannot convert because image has not been loaded.')
         if n_twoth is None:
-            n_twoth = self.image.dim1
+            n_twoth = self.image.shape[-1]
         if n_gamma is None:
-            n_gamma = self.image.dim2
+            n_gamma = self.image.shape[-2]
 
         # determine range of twoth and gamma
         self.relim()
@@ -145,7 +145,7 @@ class AreaDetectorImage(object):
 
         # perform interpolation
         f = RegularGridInterpolator(
-            (np.arange(self.image.dim2), np.arange(self.image.dim1)),
+            (np.arange(self.image.shape[-2]), np.arange(self.image.shape[-1])),
             self.image.data,
             method='nearest',
             bounds_error=False,
@@ -157,9 +157,9 @@ class AreaDetectorImage(object):
         self.data_converted = new
 
     def gridline(self, angle_deg, axis='twoth', delta_deg=0.1):
-        """GADDS image上にプロットできる等2θ, 等γのグリッド線のデータを取り出す。
+        """Extracts data for grid lines of constant 2θ and constant γ that can be plotted on a GADDS image.
 
-        Note: self.alphaやself.distanceを手動で変更した場合、gridlineの前にself.relim()を実行する必要がある。"""
+        Note: If self.alpha or self.distance are changed manually, self.relim() must be executed before gridline."""
         angle = np.deg2rad(angle_deg)
         delta = np.deg2rad(delta_deg)
         if axis == 'twoth':
@@ -172,47 +172,47 @@ class AreaDetectorImage(object):
             rows, cols = self.angles_to_rowcol(np.arange(self.limits[0], self.limits[1], delta), angle)
         else:
             raise ValueError('unknown axis: %s' % axis)
-        idx = (0 <= rows) & (rows < self.image.dim2) & (0 <= cols) & (cols < self.image.dim1)
+        idx = (0 <= rows) & (rows < self.image.shape[-2]) & (0 <= cols) & (cols < self.image.shape[-1])
         return cols[idx], rows[idx]
 
     def load_headers(self):
-        """image fileのヘッダーから必要なパラメーターを読み込む"""
+        """Reads necessary parameters from the image file header."""
         image = self.image
         if isinstance(image, BrukerImage):
-            """gfrmファイルにはversion 86と100がある（メモ帳で開いた先頭部分で確認可能）。
-            version 86についてはGADDS付属のgaddsref.pdfに詳しく説明があるが、100の場合は説明がない。
-            ここでは、DIFFRAC.EVAで開いた時に表示される値から推察されるパラメーターを設定している。
+            """gfrm files have versions 86 and 100 (can be confirmed at the beginning of the file when opened with a text editor).
+            Version 86 is described in detail in gaddsref.pdf included with GADDS, but there is no explanation for version 100.
+            Here, the parameters are inferred from the values displayed when opened with DIFFRAC.EVA.
             """
             if 'UNWARPED' not in image.header['TYPE']:
                 logger.warning('This frame has NOT been UNWARPED (corrected), and may contain some error.', stack_info=True)
             self.alpha = np.deg2rad(float(image.header['ANGLES'].split()[0]))
 
             # CENTER
-            # ver 86: x, yの2つの値が記録されている。
-            # ver100: 値が4つあるが、後半の2つが実際に使われているようだ。
+            # ver 86: two values, x and y, are recorded.
+            # ver100: There are four values, but the latter two seem to be actually used.
             self.centerXY = tuple(float(x) for x in image.header['CENTER'].split()[-2:])
 
             # linear scale and offset
             if 'LINEAR' in image.header:
                 self.scale, self.offset = (float(x) for x in image.header['LINEAR'].split()[:2])
 
-            # PIXPERCM: 1cm辺りのピクセル数（フレームが512x512の場合）
-            m = re.search('PIXPERCM:([0-9\.]+)', image.header['DETTYPE'])
+            # PIXPERCM: Number of pixels per cm (when the frame is 512x512)
+            m = re.search('PIXPERCM:([0-9\\.]+)', image.header['DETTYPE'])
             if m:
                 pixpercm = float(m.groups()[0])
             else:
                 try:
-                    # version 100の場合、DETTYPEの書式は 「名前 pixpercm cmtogrid （以降不明）」になるようだ。
+                    # For version 100, the DETTYPE format seems to be "name pixpercm cmtogrid (and so on)".
                     pixpercm = float(image.header['DETTYPE'].split()[1])
                 except (ValueError, IndexError):
                     pixpercm = DETTYPE[image.header['DETTYPE']][0]
             nrows, ncols = image.data.shape
             self.densityXY = (pixpercm * ncols / 512, pixpercm * nrows / 512)
 
-            # detector distance（試料と検出面の距離）
-            # DISTANCフィールドの一つ目の数字に、CMTOGRIDの値を加えたもの。
+            # detector distance (distance between sample and detector plane)
+            # The value of CMTOGRID is added to the first number in the DISTANC field.
             distanc = float(image.header['DISTANC'].split()[0])
-            m = re.search('CMTOGRID:([0-9\.]+)', image.header['DETTYPE'])
+            m = re.search('CMTOGRID:([0-9\\.]+)', image.header['DETTYPE'])
             if m:
                 cmtogrid = float(m.groups()[0])
             else:
